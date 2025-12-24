c3aaa6e1a247f52a46210986a307b24abe046c1b
VictorRoma01 <vhrm110700@hotmail.com>
2025-12-24 16:03:44 -0600

    Create class to connect to the database

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -14,7 +14,11 @@ VALUES ('Meche'),
 CREATE TABLE tasks (
     id_task SERIAL PRIMARY KEY,
     description varchar(255) NOT NULL,
+<<<<<<< Updated upstream:docker/postgres/init.sql
     asignee int REFERENCES members(id)
+=======
+    asignee int REFERENCES empleado(id)
+>>>>>>> Stashed changes:src/db/init.sql
 );
 
 INSERT INTO tasks(description,asignee)
@@ -27,3 +31,8 @@ VALUES('lavar platos',1),
       ('Limpiar bano',2),
       ('Acomodar Sillones', 4),
       ('Asear perritas',5);
+
+SELECT * FROM members;
+SELECT * FROM tasks;
+
+DROP TABLE tasks;
--- src/main/java/org/rodriguez/corp/Main.java
+++ src/main/java/org/rodriguez/corp/Main.java
@@ -1,7 +1,11 @@
 package org.rodriguez.corp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Member migue = new Member("Migue");
         Member vic = new Member("Victor");
@@ -18,6 +22,20 @@ public class Main {
 
 //        Task task3 = new Task("HOLA",migue);
 //        Task task4 = new Task("Miguel",vic);
+
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "app_user";
+            String password = "app_password";
+
+            try (Connection conn = DriverManager.getConnection(url, user, password)) {
+                System.out.println("Connected to PostgreSQL");
+
+                conn.
+            }
+
+
+
+
     }
 
 
